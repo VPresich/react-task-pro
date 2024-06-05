f4fda8ee6982e2433d00e505f40e2e956913646f
Valentyna Aleksandrova <111353667+VPresich@users.noreply.github.com>
2024-06-05 23:23:11 +0200

    Initial commit

---
+++ src/jsx/components/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+import { DiApple } from 'react-icons/di';
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      <h1>
+        <DiApple /> Vite + React Template
+      </h1>
+    </div>
+  );
+}
