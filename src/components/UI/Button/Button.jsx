1597e2d8743c8d9009a3e158aaab9b727fabe15b
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-06-06 15:44:11 +0300

    add separator component, too sizes of button

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,31 +1,30 @@
 import { useSelector } from 'react-redux';
 import css from './Button.module.css';
 import { selectTheme } from '../../../redux/auth/selectors';
-import { clsx } from 'clsx';
+import clsx from 'clsx';
 
-export default function Button({ icon, text }) {
+export default function Button({ icon, text, big }) {
   const theme = useSelector(selectTheme);
-//   const theme = 'Violet';
+
   return (
-    <button className={clsx(css.btn, css[theme.toLowerCase()])}>
-          {icon &&
-              <div className={clsx(css.iconContainer, css[theme.toLowerCase()])}>
-                  <svg
-                        className={clsx(css.icon, css[theme.toLowerCase()])}
-                        width="14"
-                        height="14"
-                        aria-label="btn icon"
-                        stroke='red'
-                    >
-                        <use href={`./img/icons/icons.svg#${icon}`} />
-                  </svg>
-              </div>
-          }
+    <button className={clsx(css.btn, css[theme.toLowerCase()], { [css.big]: big })}>
+      {icon && (
+        <div className={clsx(css.iconContainer, css[theme.toLowerCase()])}>
+          <svg
+            className={clsx(css.icon, css[theme.toLowerCase()])}
+            width="14"
+            height="14"
+            aria-label="btn icon"
+          >
+            <use href={`./img/icons/icons.svg#${icon}`} />
+          </svg>
+        </div>
+      )}
       {text && <span className={css.text}>{text}</span>}
     </button>
   );
 }
 
-
 //Usage example
-//  <Button icon='icon-plus' text='my text here' />
+//  <Button icon='icon-plus' text='my text here' />  => height = 49px
+//  <Button icon='icon-plus' text='my text here' big={true} />  => height = 56px
---
+++ src/components/UI/Separator/Separator.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+import css from './Separator.module.css'
+import { selectTheme } from '../../../redux/auth/selectors';
+
+import clsx from 'clsx'
+export default function Separator() {
+    const theme = useSelector(selectTheme);
+//   const theme = 'Violet';
+  return (
+    <div className={clsx(css.separator, css[theme.toLowerCase()])}></div>
+  )
+}
