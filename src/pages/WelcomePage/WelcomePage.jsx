368193de6dce1eb8a62b9d4b5eac2488a5efb6dc
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-06 12:45:43 +0200

    Update WelcomePage.jsx

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,12 +3,39 @@ import DocumentTitle from '../../components/DocumentTitle';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 // import css from './WelcomePage.module.css';
 
+import Logo from '../../components/Logo/Logo';
+
 export default function WelcomePage() {
   return (
     <>
       <DocumentTitle>Welcome</DocumentTitle>
-      <h2>Welcome page</h2>
-      <RegisterForm />
+
+      <Logo />
+      <div className={css.welcomeWrapper}>
+        <div className={css.welcomeContent}>
+          <img
+            src={WelcomeAvatar}
+            alt="avatar"
+            width="420"
+            height="420"
+            className={avatar}
+            loading="lazy"
+          />
+          <h1 className={css.mainTitle}>Task Pro</h1>
+          <p className={css.text}>
+            Supercharge your productivity and take control of your tasks with
+            Task Pro - Don't wait, start achieving your goals now!
+          </p>
+          <div className={css.linkWrapper}>
+            <RegisterForm
+              to="/registration"
+              className={css.linkRegistration}
+              Registration
+            />
+            <RegisterForm to="/login" className={css.linkLogIn} Log In />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
