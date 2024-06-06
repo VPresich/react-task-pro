f0260a15e0e043764cff30eb9758e7d0bb588cdb
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-06 16:55:01 +0200

    welcom

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,22 +1,34 @@
 import DocumentTitle from '../../components/DocumentTitle';
-// import LoginForm from '../../components/LoginForm/LoginForm';
+import LoginForm from '../../components/LoginForm/LoginForm';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import css from './WelcomePage.module.css';
 
 import Logo from '../../components/Logo/Logo';
+
+import mainDesktop1x from '../../img/main-avatar/main_desktop@1x.jpg';
+import mainDesktop2x from '../../img/main-avatar/main_desktop@2x.jpg';
 
 export default function WelcomePage() {
   return (
     <>
       <DocumentTitle>Welcome</DocumentTitle>
 
-      <Logo />
       <div className={css.welcomeWrapper}>
+        <img
+          src={mainDesktop1x}
+          srcSet={`${mainDesktop1x} 1x, ${mainDesktop2x} 2x`}
+          alt="Task Pro"
+          className={css.mainImage}
+        />
         <div className={css.welcomeContent}>
-          <h1 className={css.mainTitle}>Task Pro</h1>
+          <div className={css.logoPage}>
+            <Logo />
+            <h1 className={css.mainTitle}>Task Pro</h1>
+          </div>
           <p className={css.text}>
             Supercharge your productivity and take control of your tasks with
-            Task Pro - Don`t wait, start achieving your goals now!
+            Task
+            <br /> Pro - Don`t wait, start achieving your goals now!
           </p>
           <div className={css.linkWrapper}>
             <RegisterForm
@@ -24,7 +36,7 @@ export default function WelcomePage() {
               className={css.linkRegistration}
               Registration
             />
-            <RegisterForm to="/login" className={css.linkLogIn} Log In />
+            <LoginForm to="/login" className={css.linkLogIn} Log In />
           </div>
         </div>
       </div>
