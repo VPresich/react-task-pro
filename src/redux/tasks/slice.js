2d164170844a392ca6056f1eb2761b9623d1cff7
VPresich <valya.alexandrova.presich@gmail.com>
2024-06-07 22:49:20 +0200

    edit tasks reducer

--- src/redux/tasks/slice.js
+++ src/redux/tasks/slice.js
@@ -1,61 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { deleteTask,addTask, editTask } from "./operations";
+import { createSlice } from '@reduxjs/toolkit';
+import { deleteTask, editTask } from './operations';
 
 const tasksSlice = createSlice({
-    name: 'tasks',
-    initialState: {
-      items: [],
-      isLoading: false,
-      error: null,
-    }
-}
-.addCase(deleteTask .pending, (state) => {
-    state.error = false;
-    state.isLoading = true;
-  })
-  .addCase(deleteTask.fulfilled, (state, action) => {
-    state.isLoading = false;
-    state.items = state.items.filter(
-      (item) => item.id !== action.payload.id
-    );
-  })
-  .addCase(deleteTask.rejected, (state, action) => {
-    console.log(action);
-    state.error = true;
-    state.isLoading = false;
-  })
-  .addCase(addTask .pending, (state) => {
-    state.error = false;
-    state.isLoading = true;
-  })
-  .addCase(addTask.fulfilled, (state, action) => {
-    state.isLoading = false;
-    state.items.push(action.payload);
-  })
-  .addCase(addTask.rejected, (state, action) => {
-    console.log(action);
-    state.error = true;
-    state.isLoading = false;
-  })
-  .addCase(editTask .pending, (state) => {
-    state.error = false;
-    state.isLoading = true;
-  })
-  .addCase(editTask.fulfilled, (state, action) => {
-    state.isLoading = false;
-    const { id } = action.payload;
+  name: 'tasks',
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(deleteTask.pending, state => {
+        state.error = false;
+        state.isLoading = true;
+      })
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = state.items.filter(item => item.id !== action.payload.id);
+      })
+      .addCase(deleteTask.rejected, (state, action) => {
+        console.log(action);
+        state.error = true;
+        state.isLoading = false;
+      })
+      // .addCase(addTask.pending, state => {
+      //   state.error = false;
+      //   state.isLoading = true;
+      // })
+      // .addCase(addTask.fulfilled, (state, action) => {
+      //   state.isLoading = false;
+      //   state.items.push(action.payload);
+      // })
+      // .addCase(addTask.rejected, (state, action) => {
+      //   console.log(action);
+      //   state.error = true;
+      //   state.isLoading = false;
+      // })
+      .addCase(editTask.pending, state => {
+        state.error = false;
+        state.isLoading = true;
+      })
+      .addCase(editTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const { id } = action.payload;
         const existingItemIndex = state.items.findIndex(item => item.id === id);
         if (existingItemIndex !== -1) {
-          state.items[existingItemIndex] = action.payload; 
+          state.items[existingItemIndex] = action.payload;
         }
-        
-    
-  })
-  .addCase(editTask.rejected, (state, action) => {
-    console.log(action);
-    state.error = true;
-    state.isLoading = false;
-  })
-)
+      })
+      .addCase(editTask.rejected, (state, action) => {
+        console.log(action);
+        state.error = true;
+        state.isLoading = false;
+      });
+  },
+});
 
-export const tasksReducer = tasksSlice.reducer;
+export default tasksSlice.reducer;
