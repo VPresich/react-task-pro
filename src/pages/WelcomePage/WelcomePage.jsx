35811babfb4f83a0c5a70ce0a52df16499dcd92f
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-07 11:49:17 +0200

    button+ css+welcome

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,18 +1,15 @@
 import DocumentTitle from '../../components/DocumentTitle';
-import LoginForm from '../../components/LoginForm/LoginForm';
-import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import css from './WelcomePage.module.css';
-
-import Logo from '../../components/Logo/Logo';
-
+import CustomButton from '../../components/CustomButton/CustomButton';
+import { Link } from 'react-router-dom';
 import mainDesktop1x from '../../img/main-avatar/main_desktop@1x.jpg';
 import mainDesktop2x from '../../img/main-avatar/main_desktop@2x.jpg';
+import icon from '../../img/main-icon/app-icon.svg';
 
 export default function WelcomePage() {
   return (
     <>
       <DocumentTitle>Welcome</DocumentTitle>
-
       <div className={css.welcomeWrapper}>
         <img
           src={mainDesktop1x}
@@ -22,7 +19,9 @@ export default function WelcomePage() {
         />
         <div className={css.welcomeContent}>
           <div className={css.logoPage}>
-            <Logo />
+            <svg className="iconHome" width="48px" height="48px">
+              <image href={icon} width="48px" height="48px" />
+            </svg>
             <h1 className={css.mainTitle}>Task Pro</h1>
           </div>
           <p className={css.text}>
@@ -31,15 +30,62 @@ export default function WelcomePage() {
             <br /> Pro - Don`t wait, start achieving your goals now!
           </p>
           <div className={css.linkWrapper}>
-            <RegisterForm
-              to="/registration"
-              className={css.linkRegistration}
-              Registration
-            />
-            <LoginForm to="/login" className={css.linkLogIn} Log In />
+            <Link to="/register">
+              <CustomButton>Registration</CustomButton>
+            </Link>
+            <Link to="/login">
+              <CustomButton>LogIn</CustomButton>
+            </Link>
           </div>
         </div>
       </div>
     </>
   );
 }
+
+// import DocumentTitle from '../../components/DocumentTitle';
+// import LoginForm from '../../components/LoginForm/LoginForm';
+// import RegisterForm from '../../components/RegisterForm/RegisterForm';
+// import css from './WelcomePage.module.css';
+
+// import mainDesktop1x from '../../img/main-avatar/main_desktop@1x.jpg';
+// import mainDesktop2x from '../../img/main-avatar/main_desktop@2x.jpg';
+// import icon from '../../img/main-icon/app-icon.svg';
+
+// export default function WelcomePage() {
+//   return (
+//     <>
+//       <DocumentTitle>Welcome</DocumentTitle>
+
+//       <div className={css.welcomeWrapper}>
+//         <img
+//           src={mainDesktop1x}
+//           srcSet={`${mainDesktop1x} 1x, ${mainDesktop2x} 2x`}
+//           alt="Task Pro"
+//           className={css.mainImage}
+//         />
+//         <div className={css.welcomeContent}>
+//           <div className={css.logoPage}>
+//             <svg className="iconHome" width="48px" height="48px">
+//               <image href={icon} width="48px" height="48px" />
+//             </svg>
+//             <h1 className={css.mainTitle}>Task Pro</h1>
+//           </div>
+//           <p className={css.text}>
+//             Supercharge your productivity and take control of your tasks with
+//             Task
+//             <br /> Pro - Don`t wait, start achieving your goals now!
+//           </p>
+//           <div className={css.linkWrapper}>
+//             <RegisterForm
+//               to="/registration"
+//               className={css.linkRegistration}
+//               Registration
+//             />
+//             <LoginForm to="/login" className={css.linkLogIn} Log In />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
