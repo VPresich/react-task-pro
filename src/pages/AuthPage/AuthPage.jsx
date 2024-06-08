74e27551a6d7daaf3e6a8d8501c95514377abc51
VPresich <valya.alexandrova.presich@gmail.com>
2024-06-08 09:30:12 +0200

    added AuthNavigatuin

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import DocumentTitle from '../../components/DocumentTitle';
-import RegisterForm from '../../components/RegisterForm/RegisterForm';
-import LoginForm from '../../components/LoginForm/LoginForm';
-import css from './AuthPage.module.css';
+import AuthNavigation from '../AuthNavigation/AuthNavigation';
+// import RegisterForm from '../../components/RegisterForm/RegisterForm';
+// import LoginForm from '../../components/LoginForm/LoginForm';
+// import css from './AuthPage.module.css';
 
 export default function AuthPage() {
-  const [isRegister, setIsRegister] = useState(false);
+  // const [isRegister, setIsRegister] = useState(false);
 
   return (
     <div>
       <DocumentTitle>AuthPage</DocumentTitle>
 
-      {isRegister ? <RegisterForm /> : <LoginForm />}
+      {/* {isRegister ? <RegisterForm /> : <LoginForm />}
 
       <button onClick={() => setIsRegister(!isRegister)} className={css.button}>
         {isRegister ? 'LogIn' : 'Registration'}
-      </button>
+      </button> */}
+      <AuthNavigation />
     </div>
   );
 }
