6ee859eaa6390f6c6e0f938cc9bb000258ccc3f6
Irinaaissa <irinaalisa7506@gmail.com>
2024-06-08 23:04:09 +0300

    зробила сталі для картки для 3 фон

--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import spritePath from '../../../img/sprite.svg';
 import styles from './Card.module.css';
 import { deleteTask } from '../../../redux/tasks/operations';
 import { useDispatch } from 'react-redux';
-// import { selectAllTasks } from "../../redux/tasks/selectors";
+import { selectTheme } from '../../../redux/auth/selectors';
+import clsx from 'clsx';
 
 export default function Card({ title, description, id, priority, deadline }) {
+  const theme = useSelector(selectTheme);
   const isDeadlineSoon = deadline => {
     const deadlineDate = new Date(deadline);
     const now = new Date();
@@ -14,86 +16,88 @@ export default function Card({ title, description, id, priority, deadline }) {
     const hoursDifference = timeDifference / (1000 * 60 * 60);
     return hoursDifference <= 24;
   };
-  // const tasks = useSelector(selectAllTasks);
   const dispatch = useDispatch();
   const handleCard = () => {
     dispatch(deleteTask(id));
   };
   return (
-    <div className={styles.card} 
-       style={{ border: "none", borderLeft: "4px solid #8FA1D0" }} 
-       id={`card-${id}`}
-  >
-      <div className={styles.leftBar}>
-      <h2 className={styles.cardTitle}>{title}</h2>
-      <div className={styles.containerCard}>
-        <p className={styles.cardDescription}>{description}</p>
-      </div>
+    <div
+      className={clsx(styles.card, styles[theme])}
+      style={{ border: 'none', borderLeft: '4px solid #8FA1D0' }}
+      id={`card-${id}`}
+    >
+        <h2 className={clsx(styles.cardTitle,styles[theme])}>{title}</h2>
+        <div className={styles.containerCard}>
+          <p className={clsx(styles.cardDescription,styles[theme])}>{description}</p>
+        </div>
 
-      <div className={styles.cardLine}></div>
-      <div className={styles.cardInfo}>
-      <div className={styles.priorityContainer}>
-        <svg
-          className={styles.ellipse}
-          width="16"
-          height="16"
-          aria-label="btn icon"
-          style={{ position: "absolute", left: "3px", top:"14px",stroke:" #ccc",
-            fill: "#8fa1d0" }} 
-        >
-          <use href={`${spritePath}#icon-Ellipse`} />
-        </svg>
-        <p className={styles.cardPriority}>
-          <strong className={styles.strongPriority}>Priority:</strong>{' '}
-          {priority}
-        </p>
-      </div>
-        
-        <p className={styles.cardDeadline}>
-          <strong className={styles.strongDeadline}>Deadline:</strong>{' '}
-          {deadline}
-        </p>
-      </div>
-      <div className={styles.icon}>
-        {isDeadlineSoon(deadline) && (
+        <div className={clsx(styles.cardLine,styles[theme])}></div>
+        <div className={styles.cardInfo}>
+          <div className={styles.priorityContainer}>
+            <svg
+              className={styles.ellipse}
+              width="16"
+              height="16"
+              aria-label="btn icon"
+              style={{
+                position: 'absolute',
+                left: '3px',
+                top: '14px',
+                fill: '#8FA1D0',
+              }}
+            >
+              <use href={`${spritePath}#icon-Ellipse`} />
+            </svg>
+            <p className={clsx(styles.cardPriority,styles[theme])}>
+              <strong >Priority:</strong>{' '}
+             <span className={clsx(styles.strongPriority,styles[theme])}>{priority} </span> 
+            </p>
+          </div>
+
+          <p className={clsx(styles.cardDeadline,styles[theme])}>
+            <strong >Deadline:</strong>{' '}
+            <span className={clsx(styles.strongDeadline,styles[theme])}>{deadline}</span>
+          </p>
+        </div>
+        <div className={styles.icon}>
+          {isDeadlineSoon(deadline) && (
+            <svg
+              className={styles.bell}
+              width="16"
+              height="16"
+              aria-label="btn icon"
+            >
+              <use href={`${spritePath}#icon-bell-01`} />
+            </svg>
+          )}
+
           <svg
-            className={styles.bell}
+            className={clsx(styles.color,styles[theme])}
             width="16"
             height="16"
             aria-label="btn icon"
           >
-            <use href={`${spritePath}#icon-bell-01`} />
+            <use href={`${spritePath}#icon-arrow`} />
           </svg>
-        )}
+          <svg
+            className={clsx(styles.color,styles[theme])}
+            width="16"
+            height="16"
+            aria-label="btn icon"
+          >
+            <use href={`${spritePath}#icon-pencil`} />
+          </svg>
 
-        <svg
-          className={styles.color}
-          width="16"
-          height="16"
-          aria-label="btn icon"
-        >
-          <use href={`${spritePath}#icon-arrow`} />
-        </svg>
-        <svg
-          className={styles.color}
-          width="16"
-          height="16"
-          aria-label="btn icon"
-        >
-          <use href={`${spritePath}#icon-pencil-01`} />
-        </svg>
-
-        <svg
-          className={styles.color}
-          width="16"
-          height="16"
-          aria-label="btn icon"
-        >
-          <use href={`${spritePath}#icon-trash-04`} onClick={handleCard} />
-        </svg>
-      </div>
-      </div> 
+          <svg
+            className={clsx(styles.color,styles[theme])}
+            width="16"
+            height="16"
+            aria-label="btn icon"
+          >
+            <use href={`${spritePath}#icon-trash`} onClick={handleCard} />
+          </svg>
+        </div>
+      
     </div>
-    
   );
 }
