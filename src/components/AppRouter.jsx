0654d5f6f447bdfb73d58b559d453bbf00650206
VPresich <valya.alexandrova.presich@gmail.com>
2024-06-08 11:54:10 +0200

    Update AppRouter.jsx

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -35,9 +35,15 @@ const AppRouter = () => {
           }
         />
         <Route
+          path="home"
+          element={
+            <PrivateRoute redirectTo="/welcome" component={<HomePage />} />
+          }
+        />
+        <Route
           path="/home/:boardName"
           element={
-            <PrivateRoute redirectTo="/login" component={<ScreensPage />} />
+            <PrivateRoute redirectTo="/welcome" component={<ScreensPage />} />
           }
         />
         <Route path="*" element={<NotFoundPage />} />
@@ -47,50 +53,3 @@ const AppRouter = () => {
 };
 
 export default AppRouter;
-
-// import { lazy, Suspense } from 'react';
-// import { Routes, Route } from 'react-router-dom';
-
-// import PrivateRoute from './PrivateRoute';
-// import RestrictedRoute from './RestrictedRoute';
-
-// const AuthPage = lazy(() => import('../pages/AuthPage/AuthPage'));
-// const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
-// const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-// const ScreensPage = lazy(() => import('../pages/ScreensPage/ScreensPage'));
-// const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
-
-// const AppRouter = () => {
-//   return (
-//     //TODO Private and Restricted Route
-//     <Suspense fallback={<div>Loading page...</div>}>
-//       <Routes>
-//         <Route
-//           path="/welcome"
-//           element={
-//             <RestrictedRoute redirectTo="/" component={<WelcomePage />} />
-//           }
-//         />
-//         <Route
-//           path="/auth/:id"
-//           element={<RestrictedRoute redirectTo="/" component={<AuthPage />} />}
-//         />
-//         <Route
-//           path="/"
-//           element={
-//             <PrivateRoute redirectTo="/welcome" component={<HomePage />} />
-//           }
-//         />
-//         <Route
-//           path="/home/:boardName"
-//           element={
-//             <PrivateRoute redirectTo="/login" component={<ScreensPage />} />
-//           }
-//         />
-//         <Route path="*" element={<NotFoundPage />} />
-//       </Routes>
-//     </Suspense>
-//   );
-// };
-
-// export default AppRouter;
