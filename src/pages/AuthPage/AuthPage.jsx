371e9b924bf72cea20d895b93c9e9b1e334ba4cb
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-09 16:54:20 +0200

    Update AuthPage.jsx

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,19 +1,20 @@
-// import { useParams, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import DocumentTitle from '../../components/DocumentTitle';
-// import RegisterForm from '../../components/RegisterForm/RegisterForm';
-// import LoginForm from '../../components/LoginForm/LoginForm';
-// import css from './AuthPage.module.css';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import css from './AuthPage.module.css';
 import AuthNavigation from '../../components/AuthNavigation/AuthNavigation';
 
 export default function AuthPage() {
-  // const { id } = useParams();
-  // const isRegister = id === 'register';
+  const { id } = useParams();
+  const isRegister = id === 'register';
 
   return (
     <div>
       <DocumentTitle>AuthPage</DocumentTitle>
       <AuthNavigation />
-      {/* {isRegister ? <RegisterForm /> : <LoginForm />}
+
+      {isRegister ? <RegisterForm /> : <LoginForm />}
       <Link
         to={`/auth/${isRegister ? 'login' : 'register'}`}
         className={css.button}
