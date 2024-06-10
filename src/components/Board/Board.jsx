d4d58344a7311dcf2c29fae14594a505a22c3674
Mahavr <mahavr2122003@gmail.com>
2024-06-10 15:35:30 +0300

    add button and modal for columns

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,8 +1,29 @@
+import AddColumnBtn from '../AddColumnBtn/AddColumnBtn';
+import ModalWrapper from '../ModalWrapper/ModalWrapper';
+import ColumnModal from '../ColumnModal/ColumnModal';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectActiveBoard } from '../../redux/boards/selectors';
 
 export default function Board() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+const activeBoardId = useSelector(selectActiveBoard);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div>
       Board
+      <AddColumnBtn openModal={openModal} />
+      {isModalOpen && (
+        <ModalWrapper onClose={closeModal}>
+          <ColumnModal modalType={'add'} activeBoardId={activeBoardId} />
+        </ModalWrapper>
+      )}
     </div>
-  )
+  );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ import AppBar from '../../components/AppBar/AppBar';
 import css from './HomePage.module.css';
 import SideBar from '../../components/SideBar/SideBar';
 import Board from '../../components/Board/Board';
-import Card from '../../components/UI/Card/Card';
+
 export default function HomePage() {
   const dispatch = useDispatch();
 
@@ -35,9 +35,9 @@ export default function HomePage() {
       <div>
       <AppBar></AppBar>
       <Board/>
-      <Card title="test 1" description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime optio, 
+      {/* <Card title="test 1" description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime optio, 
 explicabo maiores enim odio ab cupiditate sit consequuntur, dolore quas voluptatibus sed iusto necessitatibus 
-at reprehenderit veniam magni aliquam cumque" priority="low" deadline="08.06.2024"/>
+at reprehenderit veniam magni aliquam cumque" priority="low" deadline="08.06.2024"/> */}
       {isLoading && <p>Loading boads...</p>}
       {error && <p>{error}</p>}
       </div>
