ea3f47dc241c1d31a04c19822f390563c7623c34
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-10 13:05:54 +0200

    npm
    "react-hook-form"  .  "@hookform/resolvers"

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,62 @@
 // import { useDispatch } from 'react-redux';
 // import { logIn } from '../../redux/auth/operations';
 // import { Formik, Form } from 'formik';
+// import FormField from '../FormField/FormField';
+// import CustomButton from '../CustomButton/CustomButton';
+// import {
+//   INITIAL_LOGIN,
+//   CAPTION_LOGIN,
+//   LABEL_EMAIL,
+//   LABEL_PASSWORD,
+// } from './constants';
+// import { feedbackSchema } from './feedback-schema';
+// import { errNotify, successNotify } from '../../notification/notification';
+// import { ERR_LOGIN, SUCCESS_LOGIN } from '../../notification/constants';
+// import css from './LoginForm.module.css';
+
+// export default function LoginForm() {
+//   const dispatch = useDispatch();
+//   const handleSubmit = (values, actions) => {
+//     dispatch(logIn(values))
+//       .unwrap()
+//       .then(() => {
+//         successNotify(SUCCESS_LOGIN);
+//         actions.resetForm();
+//       })
+//       .catch(err => {
+//         errNotify(ERR_LOGIN + err.message);
+//       });
+//   };
+
+//   return (
+//     <Formik
+//       initialValues={INITIAL_LOGIN}
+//       onSubmit={handleSubmit}
+//       validationSchema={feedbackSchema}
+//     >
+//       <Form className={css.form}>
+//         <div className={css.info}>
+//           <FormField type="email" name="email" styles={css}>
+//             {LABEL_EMAIL}
+//           </FormField>
+//           <FormField
+//             type="password"
+//             name="password"
+//             styles={css}
+//             autoComplete="current-password"
+//           >
+//             {LABEL_PASSWORD}
+//           </FormField>
+//         </div>
+//         <CustomButton type="submit">{CAPTION_LOGIN}</CustomButton>
+//       </Form>
+//     </Formik>
+//   );
+// }
+
+// import { useDispatch } from 'react-redux';
+// import { logIn } from '../../redux/auth/operations';
+// import { Formik, Form } from 'formik';
 // import Input from '../UI/Input/Input';
 // import Button from '../UI/Button/Button';
 // import { INITIAL_LOGIN } from './constants';
@@ -101,7 +157,7 @@ export default function LoginForm() {
           </div>
 
           <div className={css.info}>
-            <FormField type="email" name="email" styles={css}>
+            <FormField type="email" name="email" styles={css.fieldText}>
               {LABEL_EMAIL}
             </FormField>
             <FormField
