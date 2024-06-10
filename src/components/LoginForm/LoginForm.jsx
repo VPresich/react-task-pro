4c7cfd7b733b354afb0786e9295243a0333bc600
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-10 10:36:35 +0200

    login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,3 +1,64 @@
+// import { useDispatch } from 'react-redux';
+// import { logIn } from '../../redux/auth/operations';
+// import { Formik, Form } from 'formik';
+// import Input from '../UI/Input/Input';
+// import Button from '../UI/Button/Button';
+// import { INITIAL_LOGIN } from './constants';
+// import { feedbackSchema } from './feedback-schema';
+// import { errNotify, successNotify } from '../../notification/notification';
+// import { ERR_LOGIN, SUCCESS_LOGIN } from '../../notification/constants';
+// import css from './LoginForm.module.css';
+
+// export default function LoginForm() {
+//   const dispatch = useDispatch();
+
+//   const handleSubmit = (values, actions) => {
+//     dispatch(logIn(values))
+//       .unwrap()
+//       .then(() => {
+//         successNotify(SUCCESS_LOGIN);
+//         actions.resetForm();
+//       })
+//       .catch(err => {
+//         errNotify(ERR_LOGIN + err.message);
+//       });
+//   };
+
+//   return (
+//     <div className={css.formContent}>
+//       <Formik
+//         initialValues={INITIAL_LOGIN}
+//         onSubmit={handleSubmit}
+//         validationSchema={feedbackSchema}
+//       >
+//         <Form className={css.form}>
+//           <div className={css.buttonWrapper}>
+//             <button className={css.btnResister}>REGISTRATION</button>
+//             <button className={css.btnLogin}>LOG IN</button>
+//           </div>
+//           <div className={css.info}>
+//             <Input type="email" name="email" onPlaceholder="Enter your Email" />
+//             <Input
+//               type="password"
+//               name="password"
+//               onPlaceholder="Enter your Password"
+//               autoComplete="current-password"
+//             />
+//           </div>
+//           <Button
+//             text="Log in Now"
+//             big={false}
+//             type="submit"
+//             onClick={() => {
+//               console.log('clicked');
+//             }}
+//           />
+//         </Form>
+//       </Formik>
+//     </div>
+//   );
+// }
+
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operations';
 import { Formik, Form } from 'formik';
@@ -35,8 +96,8 @@ export default function LoginForm() {
         <Form className={css.form}>
           <div className={css.buttonWrapper}>
             {/* <AuthNavigation /> */}
-            <button className={css.btnResistr}>REGISTRATION</button>
-            <button className={css.btnLogin}>LOG IN</button>
+            <button className={css.resisterBtn}>REGISTRATION</button>
+            <button className={css.loginBtn}>LOG IN</button>
           </div>
 
           <div className={css.info}>
@@ -66,43 +127,3 @@ export default function LoginForm() {
     </div>
   );
 }
-
-// export default function LoginForm() {
-//   const dispatch = useDispatch();
-//   const handleSubmit = (values, actions) => {
-//     dispatch(logIn(values))
-//       .unwrap()
-//       .then(() => {
-//         successNotify(SUCCESS_LOGIN);
-//         actions.resetForm();
-//       })
-//       .catch(err => {
-//         errNotify(ERR_LOGIN + err.message);
-//       });
-//   };
-
-//   return (
-//     <Formik
-//       initialValues={INITIAL_LOGIN}
-//       onSubmit={handleSubmit}
-//       validationSchema={feedbackSchema}
-//     >
-//       <Form className={css.form}>
-//         <div className={css.info}>
-//           <FormField type="email" name="email" styles={css}>
-//             {LABEL_EMAIL}
-//           </FormField>
-//           <FormField
-//             type="password"
-//             name="password"
-//             styles={css}
-//             autoComplete="current-password"
-//           >
-//             {LABEL_PASSWORD}
-//           </FormField>
-//         </div>
-//         <CustomButton type="submit">{CAPTION_LOGIN}</CustomButton>
-//       </Form>
-//     </Formik>
-//   );
-// }
