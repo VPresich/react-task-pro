1e4e3b6b867cdaacafada5e5d93597c7f798f45f
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-11 13:05:59 +0200

    Update PopUp.jsx

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -1,23 +1,55 @@
-import { useDispatch } from 'react-redux';
-// import { PopUp } from '../../redux/auth/operations';
-import { Formik, Form, Field } from 'formik';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveColumn } from '../../redux/auth/operations';
+import { selectActiveColumnId, selectItems } from '../../redux/auth/selectors';
+import { Formik, Form } from 'formik';
 import css from './PopUpForm.module.css';
 import { Link } from 'react-router-dom';
 
-return (
-  <div className={css.formContent}>
-    <Formik>
-      <Form className={css.form}>
-        <div className={css.buttonWrapper}>
-          <Link to="/auth/inprogress" className={css.link}>
-            In progress
-          </Link>
-          <Link to="/auth/done" className={css.link}>
-            Done
-          </Link>
-        </div>
-      </Form>
-    </Formik>
-  </div>
-);
+const PopUp = () => {
+  const activeColumnState = useSelector(selectActiveColumnId);
+  const columns = useSelector(selectItems);
+
+  const dispatch = useDispatch();
+
+  const handleActiveColumn = id => {
+    dispatch(setActiveColumn(id));
+  };
+
+  return (
+    <div className={css.formContent}>
+      <Formik>
+        <Form className={css.form}>
+          <div className={css.buttonWrapper}>
+            <Link to="/auth/inprogress" className={css.link}>
+              In progress
+            </Link>
+            <Link to="/auth/done" className={css.link}>
+              Done
+            </Link>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+export const selectActiveColumnId = state => state.activeColumnId;
+export const selectItems = state => state.items;
+
+export default PopUp;
+
+// return (
+//   <div className={css.formContent}>
+//     <Formik>
+//       <Form className={css.form}>
+//         <div className={css.buttonWrapper}>
+//           <Link to="/auth/inprogress" className={css.link}>
+//             In progress
+//           </Link>
+//           <Link to="/auth/done" className={css.link}>
+//             Done
+//           </Link>
+//         </div>
+//       </Form>
+//     </Formik>
+//   </div>
+// );
