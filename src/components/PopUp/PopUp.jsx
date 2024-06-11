510ab55d5985d5b8d73df715e309bcbe620a1083
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-11 14:22:56 +0200

    popup

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -1,39 +1,30 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { setActiveColumn } from '../../redux/auth/operations';
-import { selectActiveColumnId, selectItems } from '../../redux/auth/selectors';
-import { Formik, Form } from 'formik';
-import css from './PopUpForm.module.css';
+import css from './PopUp.module.css';
 import { Link } from 'react-router-dom';
 
 const PopUp = () => {
-  const activeColumnState = useSelector(selectActiveColumnId);
-  const columns = useSelector(selectItems);
-
-  const dispatch = useDispatch();
-
-  const handleActiveColumn = id => {
-    dispatch(setActiveColumn(id));
-  };
-
   return (
-    <div className={css.formContent}>
-      <Formik>
-        <Form className={css.form}>
-          <div className={css.buttonWrapper}>
-            <Link to="/auth/inprogress" className={css.link}>
-              In progress
-            </Link>
-            <Link to="/auth/done" className={css.link}>
-              Done
-            </Link>
-          </div>
-        </Form>
-      </Formik>
+    <div className={css.popupDiv}>
+      <div className={css.popup.Content}>
+        <button className={css.link}>
+          <Link
+            to="/auth/inprogress"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            In progress
+          </Link>
+        </button>
+        <button className={css.link}>
+          <Link
+            to="/auth/done"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            Done
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
-export const selectActiveColumnId = state => state.activeColumnId;
-export const selectItems = state => state.items;
 
 export default PopUp;
 
