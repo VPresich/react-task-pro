0fa6dabf8fb9c7b8fbc84989e8718604262c7162
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-11 09:14:10 +0200

    update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,132 +1,121 @@
+// import { useState } from 'react';
+// import { useForm } from 'react-hook-form';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as Yup from 'yup';
 // import { useDispatch } from 'react-redux';
-// import { logIn } from '../../redux/auth/operations';
-// import { Formik, Form } from 'formik';
-// import FormField from '../FormField/FormField';
-// import CustomButton from '../CustomButton/CustomButton';
-// import {
-//   INITIAL_LOGIN,
-//   CAPTION_LOGIN,
-//   LABEL_EMAIL,
-//   LABEL_PASSWORD,
-// } from './constants';
-// import { feedbackSchema } from './feedback-schema';
-// import { errNotify, successNotify } from '../../notification/notification';
-// import { ERR_LOGIN, SUCCESS_LOGIN } from '../../notification/constants';
+// import { FiEyeOff, FiEye } from 'react-icons/fi';
+// import { Link } from 'react-router-dom';
 // import css from './LoginForm.module.css';
+// import { logIn } from '../../redux/auth/operations';
+// import toast from 'react-hot-toast';
+
+// const loginValidationSchema = Yup.object().shape({
+//   email: Yup.string()
+//     .email('Invalid email address')
+//     .required('Email is required'),
+//   password: Yup.string()
+//     .min(7, 'Password must be at least 7 characters long')
+//     .required('Password is required')
+//     .matches(
+//       /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+//       'Password must contain at least one letter and one number, only Latin letters are allowed'
+//     ),
+// });
 
 // export default function LoginForm() {
 //   const dispatch = useDispatch();
-//   const handleSubmit = (values, actions) => {
-//     dispatch(logIn(values))
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm({
+//     resolver: yupResolver(loginValidationSchema),
+//   });
+
+//   const onSubmit = data => {
+//     dispatch(logIn(data))
 //       .unwrap()
 //       .then(() => {
-//         successNotify(SUCCESS_LOGIN);
-//         actions.resetForm();
+//         toast.success('Welcome back!', {
+//           duration: 2000,
+//         });
 //       })
-//       .catch(err => {
-//         errNotify(ERR_LOGIN + err.message);
+//       .catch(error => {
+//         toast.error(
+//           "It's time to drink water, but your email or password is incorrect🙈",
+//           {
+//             duration: 4000,
+//           }
+//         );
 //       });
 //   };
 
 //   return (
-//     <Formik
-//       initialValues={INITIAL_LOGIN}
-//       onSubmit={handleSubmit}
-//       validationSchema={feedbackSchema}
-//     >
-//       <Form className={css.form}>
-//         <div className={css.info}>
-//           <FormField type="email" name="email" styles={css}>
-//             {LABEL_EMAIL}
-//           </FormField>
-//           <FormField
-//             type="password"
-//             name="password"
-//             styles={css}
-//             autoComplete="current-password"
-//           >
-//             {LABEL_PASSWORD}
-//           </FormField>
-//         </div>
-//         <CustomButton type="submit">{CAPTION_LOGIN}</CustomButton>
-//       </Form>
-//     </Formik>
-//   );
-// }
-
-// import { useDispatch } from 'react-redux';
-// import { logIn } from '../../redux/auth/operations';
-// import { Formik, Form } from 'formik';
-// import Input from '../UI/Input/Input';
-// import Button from '../UI/Button/Button';
-// import { INITIAL_LOGIN } from './constants';
-// import { feedbackSchema } from './feedback-schema';
-// import { errNotify, successNotify } from '../../notification/notification';
-// import { ERR_LOGIN, SUCCESS_LOGIN } from '../../notification/constants';
-// import css from './LoginForm.module.css';
-
-// export default function LoginForm() {
-//   const dispatch = useDispatch();
-
-//   const handleSubmit = (values, actions) => {
-//     dispatch(logIn(values))
-//       .unwrap()
-//       .then(() => {
-//         successNotify(SUCCESS_LOGIN);
-//         actions.resetForm();
-//       })
-//       .catch(err => {
-//         errNotify(ERR_LOGIN + err.message);
-//       });
-//   };
-
-//   return (
-//     <div className={css.formContent}>
-//       <Formik
-//         initialValues={INITIAL_LOGIN}
-//         onSubmit={handleSubmit}
-//         validationSchema={feedbackSchema}
-//       >
-//         <Form className={css.form}>
-//           <div className={css.buttonWrapper}>
-//             <button className={css.btnResister}>REGISTRATION</button>
-//             <button className={css.btnLogin}>LOG IN</button>
-//           </div>
-//           <div className={css.info}>
-//             <Input type="email" name="email" onPlaceholder="Enter your Email" />
-//             <Input
-//               type="password"
-//               name="password"
-//               onPlaceholder="Enter your Password"
-//               autoComplete="current-password"
+//     <div className={css.container}>
+//       <div className={css.section}>
+//         <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
+//           <div className={css.field}>
+//             <label className={css.email}>Email</label>
+//             <input
+//               className={errors.email ? css.errorInput : css.firstInput}
+//               type="text"
+//               placeholder="Enter your email"
+//               {...register('email')}
 //             />
+//             {errors.email && (
+//               <p className={css.error}>{errors.email.message}</p>
+//             )}
 //           </div>
-//           <Button
-//             text="Log in Now"
-//             big={false}
-//             type="submit"
-//             onClick={() => {
-//               console.log('clicked');
-//             }}
-//           />
-//         </Form>
-//       </Formik>
+//           <div className={css.field}>
+//             <label className={css.password}>Password</label>
+//             <div className={css.toggle}>
+//               <input
+//                 className={errors.password ? css.errorIn : css.secondInput}
+//                 type={showPassword ? 'text' : 'password'}
+//                 placeholder="Enter your password"
+//                 {...register('password')}
+//               />
+//               <div
+//                 onClick={() => setShowPassword(!showPassword)}
+//                 className={css.iconOne}
+//               >
+//                 {showPassword ? <FiEye size={20} /> : <FiEyeOff size={20} />}
+//               </div>
+//             </div>
+//             {errors.password && (
+//               <p className={css.error}>{errors.password.message}</p>
+//             )}
+//           </div>
+
+//           <button type="submit" className={css.button}>
+//             Log In
+//           </button>
+//           <p className={css.text}>
+//             Don`t have an account?
+//             <Link to="/auth/register" className={css.link}>
+//               Registration
+//             </Link>
+//           </p>
+//         </form>
+//       </div>
 //     </div>
 //   );
 // }
-// import AuthNavigation from '../../components/AuthNavigation/AuthNavigation';
+
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operations';
-import { Formik, Form, Field } from 'formik'; // import Field отдельно
+import { Formik, Form, Field } from 'formik';
 import Button from '../UI/Button/Button';
 import { INITIAL_LOGIN, LABEL_EMAIL, LABEL_PASSWORD } from './constants';
 import { feedbackSchema } from './feedback-schema';
 import { errNotify, successNotify } from '../../notification/notification';
 import { ERR_LOGIN, SUCCESS_LOGIN } from '../../notification/constants';
 import css from './LoginForm.module.css';
+import { Link } from 'react-router-dom';
 
 function CustomField({ name, type, placeholder, className }) {
-  // remove children from here. it's no longer needed as we have placeholder.
   return (
     <Field
       type={type}
@@ -160,19 +149,24 @@ export default function LoginForm() {
       >
         <Form className={css.form}>
           <div className={css.buttonWrapper}>
-            {/* <AuthNavigation /> */}
-            <button className={css.registerBtn}>Registration</button>
-            <button className={css.loginBtn}>Log In</button>
+            <Link to="/auth/register" className={css.link}>
+              Registration
+            </Link>
+            <Link to="/auth/login" className={css.link}>
+              Log In
+            </Link>
+            {/* <button className={css.registerBtn}>Registration</button>
+            <button className={css.loginBtn}>Log In</button> */}
           </div>
           <div className={css.info}>
             <CustomField
-              type="email" // changed type from text to email
+              type="email"
               name="email"
-              placeholder={LABEL_EMAIL} // use LABEL_EMAIL directly as placeholder
+              placeholder={LABEL_EMAIL}
               className={css.linkEmail}
             />
             <CustomField
-              type="password" // changed type from text to password
+              type="password"
               name="password"
               placeholder={LABEL_PASSWORD}
               autoComplete="current-password"
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,3 +1,149 @@
+// import { useState } from 'react';
+// import { useForm } from 'react-hook-form';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as Yup from 'yup';
+// import { useDispatch } from 'react-redux';
+// import { FiEyeOff, FiEye } from 'react-icons/fi';
+// import { Link } from 'react-router-dom';
+// import css from './RegisterForm.module.css';
+// import { register as userRegister } from '../../redux/auth/operations';
+// import toast from 'react-hot-toast';
+
+// const registrationValidationSchema = Yup.object({
+//   email: Yup.string()
+//     .required('Email is required')
+//     .email('Invalid email address'),
+//   password: Yup.string()
+//     .required('Password is required')
+//     .min(7, 'Password must be at least 7 characters long')
+//     .matches(
+//       /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+//       'Password must contain at least one letter and one number, only Latin letters are allowed'
+//     ),
+//   repeatPassword: Yup.string()
+//     .oneOf([Yup.ref('password')], 'Passwords must match')
+//     .required('Please confirm your password'),
+// });
+
+// export default function RegistrationForm() {
+//   const dispatch = useDispatch();
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//     reset,
+//   } = useForm({
+//     resolver: yupResolver(registrationValidationSchema),
+//   });
+
+//   const onSubmit = data => {
+//     const name = data.email.split('@')[0];
+//     const email = data.email;
+//     const password = data.password;
+//     dispatch(userRegister({ name, email, password }))
+//       .unwrap()
+//       .then(() => {
+//         toast.success('Registration successful! Welcome aboard! 🎉', {
+//           duration: 2000,
+//         });
+//         reset();
+//       })
+//       .catch(error => {
+//         toast.error('Oops!Failed to registration', { duration: 4000 });
+//       });
+//   };
+
+//   return (
+//     <div className={css.container}>
+//       <div className={css.section}>
+//         <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
+//           <div className={css.field}>
+//             <label className={css.email}>Name</label>
+//             <input
+//               className={errors.email ? css.errorInput : css.firstInput}
+//               type="text"
+//               placeholder="Enter your name"
+//               {...register('name')}
+//             />
+//             {errors.email && (
+//               <p className={css.error}>{errors.email.message}</p>
+//             )}
+//           </div>
+
+//           <div className={css.field}>
+//             <label className={css.email}>Email</label>
+//             <input
+//               className={errors.email ? css.errorInput : css.firstInput}
+//               type="text"
+//               placeholder="Enter your email"
+//               {...register('email')}
+//             />
+//             {errors.email && (
+//               <p className={css.error}>{errors.email.message}</p>
+//             )}
+//           </div>
+//           <div className={css.field}>
+//             <label className={css.password}>Password</label>
+//             <div className={css.toggle}>
+//               <input
+//                 className={errors.password ? css.errorIn : css.secondInput}
+//                 type={showPassword ? 'text' : 'password'}
+//                 placeholder="Enter your password"
+//                 {...register('password')}
+//               />
+//               <div
+//                 onClick={() => setShowPassword(!showPassword)}
+//                 className={css.iconOne}
+//               >
+//                 {showPassword ? <FiEye size={14} /> : <FiEyeOff size={14} />}
+//               </div>
+//             </div>
+//             {errors.password && (
+//               <p className={css.error}>{errors.password.message}</p>
+//             )}
+//           </div>
+//           <div className={css.field}>
+//             <label className={css.repeat}>Repeat Password</label>
+//             <div className={css.toggle}>
+//               <input
+//                 className={errors.repeatPassword ? css.errorIn : css.thirdInput}
+//                 type={showRepeatPassword ? 'text' : 'password'}
+//                 placeholder="Repeat password"
+//                 {...register('repeatPassword')}
+//               />
+//               <div
+//                 onClick={() => setShowRepeatPassword(!showRepeatPassword)}
+//                 className={css.iconTwo}
+//               >
+//                 {showRepeatPassword ? (
+//                   <FiEye size={20} />
+//                 ) : (
+//                   <FiEyeOff size={20} />
+//                 )}
+//               </div>
+//             </div>
+//             {errors.repeatPassword && (
+//               <p className={css.error}>{errors.repeatPassword.message}</p>
+//             )}
+//           </div>
+//           <button type="submit" className={css.button}>
+//             Registration
+//           </button>
+//           <p className={css.text}>
+//             Already have an account?
+//             <Link to="/auth/login" className={css.link}>
+//               Log In
+//             </Link>
+//           </p>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
 import { useDispatch } from 'react-redux';
 import { register } from '../../redux/auth/operations';
 import { Formik, Form } from 'formik';
